61e791901db2f5f078f9b21602c02a0616a1c268
byaliego <yogun@student.42heilbronn.de>
2022-09-02 02:08:00 +0200

    first commit

---
+++ src/server.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/02 00:43:56 by yogun             #+#    #+#             */
+/*   Updated: 2022/09/02 01:23:52 by yogun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/minitalk.h"
+
+// Why would I use signal while there is a better version of it which is sigaction yay
+int main()
+{
+    return 0;
+}
