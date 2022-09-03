c323445cb59872f4e936e0f7a2cbd99ff7866612
byaliego <yogun@student.42heilbronn.de>
2022-09-04 00:28:07 +0200

    librar update

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,12 +6,22 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 00:46:00 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/02 22:51:55 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/04 00:10:07 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINI_TALK_H
+# define MINI_TALK_H
+
 # include <signal.h>
 # include <unistd.h>
-# include <string.h>
 # include <stdlib.h>
-# include <stdio.h>
+
+
+int		ft_atoi(const char *str);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
