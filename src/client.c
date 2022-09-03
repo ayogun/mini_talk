67379e870988304d8c9c0e87f97fa4375f7808a5
byaliego <yogun@student.42heilbronn.de>
2022-09-03 23:38:51 +0200

    server and client has been extended

--- src/client.c
+++ src/client.c
@@ -6,23 +6,51 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 00:44:06 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/03 00:54:43 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/03 16:52:40 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
 // kill() is used to send signal to process
+void	send_signals(char *message, int pid)
+{
+   int	i;
+   int	shift;
+
+   shift = -1;
+   i = 0;
+   while (message[i])
+   {
+   	while (++shift < 8)
+   	{
+   		if (message[i] & (0x80 >> shift))
+   		{
+   			if (kill(pid, SIGUSR2) == -1)
+   				exit(1);
+   		}
+   		else
+   		{
+   			if (kill(pid, SIGUSR1) == -1)
+   				exit (1);
+   		}
+   		usleep(3);
+   	}
+   	i++;
+   }
+}
+
 int	main(int argc, char *argv[])
 {
-   int	pid;
+   int		pid;
 
-   if (argc != 2)
+   if (argc != 3)
    {
    	printf("client: invalid arguments\n");
+   	printf("\tcorrect format [./%s SERVER_PID MESSAGE\n]", argv[0]);
    	exit(EXIT_FAILURE);
    }
    pid = atoi(argv[1]);
-   kill(pid, SIGUSR1);
+   send_signals(argv[2], pid);
    return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,25 +6,56 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 00:43:56 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/02 22:51:25 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/03 22:34:40 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
 // Why would I use signal while there is a better version of it which is sigaction YAY
-void	handler_sigusr1(int signum)
+void	handler_sigusr(int signum)
 {
-   printf("signal %d received.\n", signum);
+   static char	c = 0xFF;
+   static int	bits = 0;
+
+   if (signum == SIGUSR1)
+   {
+   	printf("0");
+   	c ^= 0x80 >> bits;
+   }
+   else if (signum == SIGUSR2)
+   {
+   	printf("1");
+   	c |= 0x80 >> bits;
+   }
+   bits++;
+   if (bits == 8)
+   {
+   	printf("-> %c\n", c);
+   	bits = 0;
+   	c = 0xFF;
+   }
 }
 
-int	main()
+// If SA_SIGINFO is set and the signal is caught, the signal-catching function shall be entered as:
+// void func(int signo, siginfo_t *info, void *context);
+// STDOUT_FILENO	1	/* standard output file descriptor */
+int	main(void)
 {
-   pid_t	pid;
+	struct sigaction	sa;
+	pid_t				pid_server;
 
-   pid = getpid();
-   printf("PID: %d\n", pid);
-   signal(SIGUSR1, handler_sigusr1);
-   while (1)
-   	pause();
+	pid_server = getpid();
+	ft_putstr_fd("Server PID: ", STDOUT_FILENO);
+	ft_putnbr_fd(pid_server, STDOUT_FILENO);
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	sa.sa_sigaction = action;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
+
+
