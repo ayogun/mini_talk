8db52a54a7fb76bd64a1b2d938cdd43b4f1dfd63
byaliego <yogun@student.42heilbronn.de>
2022-09-04 20:11:07 +0200

    Structure is defined instead of global variables

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 00:46:00 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/04 00:10:07 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/04 19:18:02 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_message
+{
+	int		char_value;
+	int		bit_pos;
+	pid_t	pid;
+}			t_message;
 
 int		ft_atoi(const char *str);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 size_t	ft_strlen(const char *s);
+
 
 #endif
--- src/server.c
+++ src/server.c
@@ -6,38 +6,49 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 00:43:56 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/04 13:03:49 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/04 19:54:31 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
+#include <stdio.h>
+
+void	var_init(t_message *var)
+{
+	var->char_value = 0;
+	var->bit_pos = 0;
+}
 
 // Why would I use signal while there is a better version of it which is sigaction YAY.
-// Sigaction is more ortable.
+// Sigaction is more ortable. I use struct instead of global variable.
 static void	action(int sig, siginfo_t *info, void *context)
 {
-	static int				i = 0;
-	static unsigned char	c = 0;
 	pid_t					pid_client;
+	static t_message	message;
+
+
 
 	(void)context;
+	if (message.pid != info->si_pid)
+		var_init(&message);
+
+	message.pid = info->si_pid;
 	if (info->si_pid)
 		pid_client = info->si_pid;
-	c = c | (sig == SIGUSR1);
-	if (++i == 8)
+	(message.char_value) = (message.char_value) | (sig == SIGUSR1);
+	if (++(message.bit_pos) == 8)
 	{
-		i = 0;
-		if (!c)
+		if (!(message.char_value))
 		{
 			kill(pid_client, SIGUSR1);
 			return ;
 		}
-		ft_putchar_fd(c, 1);
-		c = 0;
+		ft_putchar_fd((message.char_value), 1);
 		kill(pid_client, SIGUSR2);
+		var_init(&message);
 	}
 	else
-		c = c << 1;
+		(message.char_value) = (message.char_value) << 1;
 }
 
 // If SA_SIGINFO is set and the signal is caught, the signal-catching function shall be entered as:
@@ -47,7 +58,7 @@ int	main(void)
 {
 	struct sigaction	sa;
 	pid_t				pid_server;
-
+	
 	pid_server = getpid();
 	ft_putstr_fd("Server PID: ", STDOUT_FILENO);
 	ft_putnbr_fd(pid_server, STDOUT_FILENO);
